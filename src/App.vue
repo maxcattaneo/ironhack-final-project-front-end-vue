cfee5c4d0affc0d544c26446054a73df8c0ebec0
Maximiliano Cattáneo <maximilianodamiancattaneo@gmail.com>
2023-01-05 20:20:45 +0100

    formulario de registro

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <section>
+  <section class="flex">
     <router-view class="app-main" />
+    <div>
+      <img class="h-screen " src="https://i.pinimg.com/originals/b9/b7/d0/b9b7d05effe72f4abda5f6b43d5f60e2.jpg" alt="">
+    </div>
+    <div class="flex flex-col justify-center items-center mx-auto">
+      <h1 class="text-3xl font-bold m-10">Cards .</h1>
+      <LoginForm v-if="login"/>
+      <RegisterForm v-else/>
+      <button @click="change" class="bg-white  hover:text-black text-gray-400 w-[300px]   font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+      Sign up
+    </button>
+    </div>
   </section>
 </template>
 
 <script setup>
+import {ref} from "vue";
 import { onMounted } from "vue";
 import { storeToRefs } from "pinia";
 import { useRouter } from "vue-router";
 import { useUserStore } from "./store/user.js";
+import LoginForm from "./components/LoginForm.vue"; 
+import RegisterForm from "./components/RegisterForm.vue";
 
+const login = ref(true);
 const router = useRouter();
 const userStore = useUserStore();
 const { user } = storeToRefs(userStore);
+
+function change() {
+  login.value = !login.value;
+}
 
 onMounted(async () => {
   try {
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <form class="w-full max-w-xs">
+  <div class="mb-4">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+      Email
+    </label>
+    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" v-model="email">
+  </div>
+  <div class="mb-6">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+      Password
+    </label>
+    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="* * * * * * * * *" v-model="password">
+  </div>
+  <div class="flex flex-col items-center justify-between">
+    <button class="bg-white border-solid border-2 border-gray-400 w-[300px]  hover:bg-gray-400 text-black  font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+      Log in
+    </button>
+  </div>
+</form>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const email = ref("");
+const password = ref("");
+
+</script>
+
+<style>
+
+</style>
